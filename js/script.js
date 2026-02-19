29a6b847e7fc475fde31bf3371635931984989c7
ment620-c <a.chieve.ment620@gmail.com>
2026-02-19 10:41:11 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// hamburger
+$('#hamburger').click(function(){
+ $('#line1').toggleClass('line_1');
+ $('#line2').toggleClass('line_2');
+ $('#line3').toggleClass('line_3');
+ $('nav').toggleClass('in');
+});
